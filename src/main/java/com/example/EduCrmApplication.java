ac9e1274e526b799a3b6ad838f781373502cf0d3
Husen <husenteshayiv@gmail.com>
2024-04-03 12:39:02 +0500

    project started

---
+++ src/main/java/com/example/EduCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCrmApplication.class, args);
+	}
+
+}
